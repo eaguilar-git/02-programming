603515d7b0c42fddfe813c41a9a198b88d9d7f25
eaguilar-git <aguilared.96@gmail.com>
2025-01-25 20:12:51 -0500

    uploading this

---
+++ other class notes Edgar.R
@@ -0,0 +1,25 @@
+#+ sometimes you only want to upload one thing from a library. like 
+#+ readr::read_csv. we only want to use the read csv function. sometimes
+#+ if you upload the whole library that might mess up.
+#+ 
+#+ 
+#+ 
+#+ 
+#+ 
+#+ object you are asinging output to.
+#+ 
+#+ df <- read csv ("file csv") is an assignment operator. it assings the right
+#+ to the left. 
+#+ 
+#+ object df that takes output from...
+#+ 
+#+ 
+#+ ### extracting information from objects
+#+ $ - extract element. example: object$subitem#subitem
+#+ 
+#+ 
+#+ major functionality of mark down you can type and code at the same time
+#+ 
+#+ 
+#+ a YAML needs to be done the same way every time. start in first line
+#+ --- at the end and --- at begining. 3 dashes.
